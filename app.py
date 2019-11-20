758429e0c1a0053ab22cf5a090a49fe76fe1a6e3
Deshan Basura <deshanbasura3@gmail.com>
2019-11-20 10:13:42 -0500

    Fix Bugs

--- app.py
+++ app.py
@@ -11,12 +11,13 @@ tree = html.fromstring(page.content)
 curr_name = tree.xpath('//a[@class="currency-name-container link-secondary"]/text()')
 curr_price = tree.xpath('//a[@class="price"]/text()')
 curr_volume = tree.xpath('//a[@class="volume"]/text()')
-curr_change = tree.xpath('//td[@class="no-wrap percent-change  text-right negative_change"]/text()')
+# curr_change = tree.xpath('//td[@class="no-wrap percent-change text-right negative_change"]/text()')
 
 
 # making a dictionary on gatherd data
-paired_list = list(zip(curr_name,curr_price))
+paired_list = list(zip(curr_name,curr_price,curr_volume))
+# print(paired_list)
 
-tabulate_table = tabulate(paired_list,headers=["Currency", "Price","Volume", "Change (24h)"])
+tabulate_table = tabulate(paired_list,headers=["Currency", "Price","Volume"])
 print(tabulate_table)
 
